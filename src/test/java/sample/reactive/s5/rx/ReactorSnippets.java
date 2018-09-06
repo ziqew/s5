91e3e1f99f8b9c7c3a0be9cad272a8704c08a18b
gongwenwei <gongwenwei@kedacom.com>
2018-09-06 08:39:57 +0800

    add reactive

---
+++ src/test/java/sample/reactive/s5/rx/ReactorSnippets.java
@@ -0,0 +1,45 @@
+package sample.reactive.s5.rx;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReactorSnippets {
+
+    private static List<String> words = Arrays.asList(
+            "the",
+            "quick",
+            "brown",
+            "fox",
+            "jumped",
+            "over",
+            "the",
+            "lazy",
+            "dog"
+    );
+
+    @Test
+    public void simpleCreation() {
+        Flux<String> fewWords = Flux.just("Hello", "World");
+        Flux<String> manyWords = Flux.fromIterable(words);
+
+        fewWords.subscribe(System.out::println);
+        System.out.print("-------\n");
+        manyWords.subscribe(System.out::println);
+    }
+
+    @Test
+    public void findingMissingLetter() {
+        Flux<String> manyLetters = Flux
+                .fromIterable(words)
+                .flatMap(word -> Flux.fromArray(word.split("")))
+                .distinct()
+                .sort()
+                .zipWith(Flux.range(1, Integer.MAX_VALUE),
+                        (string, count) -> String.format("%2d. %s", count, string));
+
+        manyLetters.subscribe(System.out::println);
+    }
+}
