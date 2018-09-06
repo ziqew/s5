a59d4b6e58b82e9fb9886cfb12ffc5fe1bb4c673
gongwenwei <gongwenwei@kedacom.com>
2018-09-06 18:32:28 +0800

    add reactive

--- src/test/java/sample/reactive/s5/rx/ReactorSnippets.java
+++ src/test/java/sample/reactive/s5/rx/ReactorSnippets.java
@@ -2,6 +2,8 @@ package sample.reactive.s5.rx;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
@@ -41,5 +43,80 @@ public class ReactorSnippets {
                         (string, count) -> String.format("%2d. %s", count, string));
 
         manyLetters.subscribe(System.out::println);
+    }
+
+    @Test
+    public void testFlux(){
+/*        Flux<String> seq1 = Flux.just("foo", "bar", "foobar");
+
+        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        Flux<String> seq2 = Flux.fromIterable(iterable);
+        seq2.subscribe(System.out::println);*/
+
+        SampleSubscriber<Integer> ss = new SampleSubscriber<Integer>();
+        Flux<Integer> ints = Flux.range(1, 4);
+
+/*        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> {System.out.println("Done");});*/
+
+        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> {System.out.println("Done");},
+                s -> ss.request(4));
+        ints.subscribe(ss);
+    }
+
+    @Test
+    public void testGenerate(){
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3*state);
+                    if (state == 10) sink.complete();
+                    return state + 1;
+                });
+    }
+
+    @Test
+    public void test1(){
+        Flux<Integer> ints = Flux.range(1, 3);
+        ints.subscribe(i -> System.out.println(i));
+        //ints.subscribe(System.out::println);
+    }
+
+    @Test
+    public void test2(){
+        Flux<Integer> ints = Flux.range(1, 4)
+                .map(i -> {
+                    if (i <= 3) return i;
+                    throw new RuntimeException("Got to 4");
+                });
+        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error: " + error));
+    }
+
+    @Test
+    public void test3(){
+        Flux<Integer> ints = Flux.range(1, 4);
+        ints.subscribe(i -> System.out.println(i),
+                error -> System.err.println("Error " + error),
+                () -> {System.out.println("Done");});
+    }
+
+    @Test
+    public void testAppendBoomError() {
+        Flux<String> source = Flux.just("foo", "bar");
+
+        StepVerifier.create(
+                appendBoomError(source))
+                .expectNext("foo")
+                .expectNext("bar")
+                .expectErrorMessage("boom")
+                .verify();
+    }
+
+    public <T> Flux<T> appendBoomError(Flux<T> source) {
+        return source.concatWith(Mono.error(new IllegalArgumentException("boom")));
     }
 }
